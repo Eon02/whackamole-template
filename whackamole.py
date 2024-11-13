2e55df3c7ac35e88ecafbb9c61e4281d955c997c
haley <tarala981@gmail.com>
2024-11-13 12:03:33 -0500

    Whackamole with implemented click, random mole generation, and grid

--- whackamole.py
+++ whackamole.py
@@ -1,22 +1,38 @@
 import pygame
-
+import random
 
 def main():
     try:
         pygame.init()
         # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
+        molecoords = (0, 0)
+
+        
         clock = pygame.time.Clock()
         running = True
         while running:
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    event.pos = pygame.mouse.get_pos()
+                    if event.pos[0] in range(molecoords[0] + 32) and event.pos[1] in range(molecoords[1] + 32):
+                        molecoords = (random.randint(0,20) * 32, random.randint(0,16) * 32)
+                        screen.blit(mole_image, mole_image.get_rect(topleft=molecoords))
+                
+                    
+            screen.fill("lightgreen")
+            for i in range(20):
+                pygame.draw.line(screen, "darkgreen", ((32 * i), 0), ((32 * i), 512))
+            for i in range(16):
+                pygame.draw.line(screen, "darkgreen", (0, 32 * i), (640, 32 * i))
+            screen.blit(mole_image, mole_image.get_rect(topleft=molecoords))
             pygame.display.flip()
             clock.tick(60)
+            
     finally:
         pygame.quit()
 
